eb03845292275eaef6c0d452439225a53b5a29c9
Maksim Fedotov <maksim.fedotov@flant.com>
2025-05-20 10:30:31 +0300

    Apply patch 015-rename-core-resources.patch
    Replace "kubevirt" with "kubevirt-internal-virtualziation" in the core resource names.

--- pkg/virt-operator/resource/generate/components/serviceaccountnames.go
+++ pkg/virt-operator/resource/generate/components/serviceaccountnames.go
@@ -1,10 +1,10 @@
 package components
 
 const (
-	ApiServiceAccountName                       = "kubevirt-apiserver"
-	ControllerServiceAccountName                = "kubevirt-controller"
-	ExportProxyServiceAccountName               = "kubevirt-exportproxy"
+	ApiServiceAccountName                       = "kubevirt-internal-virtualization-apiserver"
+	ControllerServiceAccountName                = "kubevirt-internal-virtualization-controller"
+	ExportProxyServiceAccountName               = "kubevirt-internal-virtualization-exportproxy"
 	SynchronizationControllerServiceAccountName = "kubevirt-synchronization-controller"
-	HandlerServiceAccountName                   = "kubevirt-handler"
+	HandlerServiceAccountName                   = "kubevirt-internal-virtualization-handler"
 	OperatorServiceAccountName                  = "kubevirt-operator"
 )
