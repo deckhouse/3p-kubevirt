1e967451b0b0ff2340a5bacc62164b74aafaf881
Maksim Fedotov <maksim.fedotov@flant.com>
2025-05-20 10:30:33 +0300

    Apply patch 034-allow-update-kvvmi-for-virtualization-sas.patch
    virt-controller mutating webhook silently reverts all changes to the KVVMI status field by
    unknown ServiceAccounts.
    
    This patch adds virtualization module accounts to the allowed list: `virtualization-controller` and `virtualization-api`, so virtualization-controller can update KVVMI status field.

--- pkg/virt-operator/resource/generate/components/serviceaccountnames.go
+++ pkg/virt-operator/resource/generate/components/serviceaccountnames.go
@@ -7,4 +7,7 @@ const (
 	SynchronizationControllerServiceAccountName = "kubevirt-synchronization-controller"
 	HandlerServiceAccountName                   = "kubevirt-internal-virtualization-handler"
 	OperatorServiceAccountName                  = "kubevirt-operator"
+
+	VirtualizationController = "virtualization-controller"
+	VirtualizationApi        = "virtualization-api"
 )
