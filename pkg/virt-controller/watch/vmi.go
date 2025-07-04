4c0735e1a58cc928e76781f43a6d24bb5c00fa61
Yaroslav Borbat <yaroslav.borbat@flant.com>
2025-07-04 14:18:10 +0300

    fix
    Signed-off-by: Yaroslav Borbat <yaroslav.borbat@flant.com>

--- pkg/virt-controller/watch/vmi.go
+++ pkg/virt-controller/watch/vmi.go
@@ -2224,8 +2224,7 @@ func (c *VMIController) updateVolumeStatus(vmi *virtv1.VirtualMachineInstance, v
 						isReady bool
 					)
 					for _, cs := range attachmentPod.Status.ContainerStatuses {
-						name := strings.TrimPrefix(cs.Name, "hotplug-container-disk-")
-						if volume.Name == name {
+						if volume.Name == attachmentPod.GetAnnotations()[cs.Name] {
 							uid = attachmentPod.UID
 							isReady = cs.Ready
 							break
