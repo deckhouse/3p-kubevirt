78eede127d04f72056fc4c423fbcef000ce7a4fc
Daniil Antoshin <daniil.antoshin@flant.com>
2025-10-09 13:15:31 +0300

    fix(vmbda): NFS hoplug (#26)
    Description
    That PR fix path placement for filesystem pvc.
    
    Why do we need it, and what problem does it solve?
    Thats required for read-only containers and erofs(containerv2).
    
    ---------
    
    Signed-off-by: Daniil Antoshin <daniil.antoshin@flant.com>

--- pkg/virt-controller/services/template_test.go
+++ pkg/virt-controller/services/template_test.go
@@ -4232,7 +4232,7 @@ var _ = Describe("Template", func() {
 				},
 				{
 					Name:      volumeName,
-					MountPath: "/" + volumeName,
+					MountPath: "/path/" + volumeName,
 				},
 			}))
 		})
