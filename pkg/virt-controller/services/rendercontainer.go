kubevirt/kubevirt

feat(core): read-only containers (#9)
Description
As part of the integrity control task, we make all containers read-only, including Kubevirt containers, Containerized data importer containers.

Why do we need it, and what problem does it solve?
These changes improve the ability for integrity checks. Improve security for containers.

What is the expected result?
All containers from the d8-virtualization namespace, as well as all containers running in a custom namespace, must be created read-only.

---------

Signed-off-by: Daniil Antoshin <daniil.antoshin@flant.com>

pkg/virt-controller/services/rendercontainer.go
@@ -184,10 +184,11 @@ func xdgEnvironmentVariables() []k8sv1.EnvVar {
 func securityContext(userId int64, privileged bool, requiredCapabilities *k8sv1.Capabilities) *k8sv1.SecurityContext {
 	isNonRoot := userId != 0
 	context := &k8sv1.SecurityContext{
-		RunAsUser:    &userId,
-		RunAsNonRoot: &isNonRoot,
-		Privileged:   &privileged,
-		Capabilities: requiredCapabilities,
+		RunAsUser:              &userId,
+		RunAsNonRoot:           &isNonRoot,
+		Privileged:             &privileged,
+		Capabilities:           requiredCapabilities,
+		ReadOnlyRootFilesystem: pointer.Bool(true),
 	}
 
 	if isNonRoot {

pkg/virt-controller/services/serialconsolelog.go
@@ -38,6 +38,7 @@ func generateSerialConsoleLogContainer(vmi *v1.VirtualMachineInstance, image str
 				RunAsUser:                pointer.Int64(util.NonRootUID),
 				RunAsNonRoot:             pointer.Bool(true),
 				AllowPrivilegeEscalation: pointer.Bool(false),
+				ReadOnlyRootFilesystem:   pointer.Bool(true),
 				Capabilities: &k8sv1.Capabilities{
 					Drop: []k8sv1.Capability{"ALL"},
 				},

pkg/virt-controller/services/template.go
@@ -894,6 +894,7 @@ func (t *templateService) containerForHotplugContainerDisk(ctrName, volName stri
 		Resources: hotplugContainerResourceRequirementsForVMI(vmi, t.clusterConfig),
 		SecurityContext: &k8sv1.SecurityContext{
 			AllowPrivilegeEscalation: pointer.Bool(false),
+			ReadOnlyRootFilesystem:   pointer.Bool(true),
 			RunAsNonRoot:             pointer.Bool(true),
 			RunAsUser:                &runUser,
 			SeccompProfile: &k8sv1.SeccompProfile{
@@ -950,6 +951,7 @@ func (t *templateService) RenderHotplugAttachmentPodTemplate(volumes []*v1.Volum
 					Resources: hotplugContainerResourceRequirementsForVMI(vmi, t.clusterConfig),
 					SecurityContext: &k8sv1.SecurityContext{
 						AllowPrivilegeEscalation: pointer.Bool(false),
+						ReadOnlyRootFilesystem:   pointer.Bool(true),
 						RunAsNonRoot:             pointer.Bool(true),
 						RunAsUser:                &runUser,
 						SeccompProfile: &k8sv1.SeccompProfile{
@@ -1123,6 +1125,7 @@ func (t *templateService) RenderHotplugAttachmentTriggerPodTemplate(volume *v1.V
 					Command:   command,
 					Resources: hotplugContainerResourceRequirementsForVMI(vmi, t.clusterConfig),
 					SecurityContext: &k8sv1.SecurityContext{
+						ReadOnlyRootFilesystem:   pointer.Bool(true),
 						AllowPrivilegeEscalation: pointer.Bool(false),
 						RunAsNonRoot:             pointer.Bool(true),
 						RunAsUser:                &runUser,
@@ -1233,6 +1236,7 @@ func (t *templateService) RenderExporterManifest(vmExport *exportv1.VirtualMachi
 					},
 					SecurityContext: &k8sv1.SecurityContext{
 						AllowPrivilegeEscalation: pointer.Bool(false),
+						ReadOnlyRootFilesystem:   pointer.Bool(true),
 						Capabilities:             &k8sv1.Capabilities{Drop: []k8sv1.Capability{"ALL"}},
 					},
 					Resources: vmExportContainerResourceRequirements(t.clusterConfig),
@@ -1427,6 +1431,7 @@ func generateContainerSecurityContext(selinuxType string, container *k8sv1.Conta
 	}
 	container.SecurityContext.SELinuxOptions.Type = selinuxType
 	container.SecurityContext.SELinuxOptions.Level = "s0"
+	container.SecurityContext.ReadOnlyRootFilesystem = pointer.Bool(true)
 }
 
 func generatePodAnnotations(vmi *v1.VirtualMachineInstance, config *virtconfig.ClusterConfig) (map[string]string, error) {

pkg/virt-controller/services/template_test.go
@@ -3938,6 +3938,7 @@ var _ = Describe("Template", func() {
 			runUser := int64(util.NonRootUID)
 			Expect(*pod.Spec.Containers[0].SecurityContext).To(Equal(k8sv1.SecurityContext{
 				AllowPrivilegeEscalation: pointer.Bool(false),
+				ReadOnlyRootFilesystem:   pointer.Bool(true),
 				RunAsNonRoot:             pointer.Bool(true),
 				RunAsUser:                &runUser,
 				SeccompProfile: &k8sv1.SeccompProfile{
@@ -4052,6 +4053,7 @@ var _ = Describe("Template", func() {
 			runUser := int64(util.NonRootUID)
 			Expect(*pod.Spec.Containers[0].SecurityContext).To(Equal(k8sv1.SecurityContext{
 				AllowPrivilegeEscalation: pointer.Bool(false),
+				ReadOnlyRootFilesystem:   pointer.Bool(true),
 				RunAsNonRoot:             pointer.Bool(true),
 				RunAsUser:                &runUser,
 				SeccompProfile: &k8sv1.SeccompProfile{

pkg/virt-controller/services/virtiofs.go
@@ -128,6 +128,7 @@ func securityContextVirtioFS(profile securityProfile) *k8sv1.SecurityContext {
 		RunAsGroup:               credential,
 		RunAsNonRoot:             pointer.Bool(isRestricted(profile)),
 		AllowPrivilegeEscalation: pointer.Bool(isPrivileged(profile)),
+		ReadOnlyRootFilesystem:   pointer.Bool(true),
 		Capabilities:             virtiofsCapabilities(profile),
 	}
 }

pkg/virt-operator/resource/generate/components/deployments.go
@@ -323,6 +323,19 @@ func NewApiServerDeployment(namespace, repository, imagePrefix, version, product
 	attachCertificateSecret(&deployment.Spec.Template.Spec, VirtHandlerCertSecretName, "/etc/virt-handler/clientcertificates")
 	attachProfileVolume(&deployment.Spec.Template.Spec)
 
+	volume := corev1.Volume{
+		Name: "tmp",
+		VolumeSource: corev1.VolumeSource{
+			EmptyDir: &corev1.EmptyDirVolumeSource{},
+		},
+	}
+	volumeMount := corev1.VolumeMount{
+		Name:      "tmp",
+		MountPath: "/tmp",
+	}
+	deployment.Spec.Template.Spec.Volumes = append(deployment.Spec.Template.Spec.Volumes, volume)
+	deployment.Spec.Template.Spec.Containers[0].VolumeMounts = append(deployment.Spec.Template.Spec.Containers[0].VolumeMounts, volumeMount)
+
 	pod := &deployment.Spec.Template.Spec
 	pod.ServiceAccountName = ApiServiceAccountName
 	pod.SecurityContext = &corev1.PodSecurityContext{
@@ -379,6 +392,7 @@ func NewApiServerDeployment(namespace, repository, imagePrefix, version, product
 
 	container.SecurityContext = &corev1.SecurityContext{
 		AllowPrivilegeEscalation: pointer.Bool(false),
+		ReadOnlyRootFilesystem:   pointer.Bool(true),
 		Capabilities: &corev1.Capabilities{
 			Drop: []corev1.Capability{"ALL"},
 		},
@@ -476,6 +490,7 @@ func NewControllerDeployment(namespace, repository, imagePrefix, controllerVersi
 
 	container.SecurityContext = &corev1.SecurityContext{
 		AllowPrivilegeEscalation: pointer.Bool(false),
+		ReadOnlyRootFilesystem:   pointer.Bool(true),
 		Capabilities: &corev1.Capabilities{
 			Drop: []corev1.Capability{"ALL"},
 		},
@@ -603,6 +618,7 @@ func NewOperatorDeployment(namespace, repository, imagePrefix, version, verbosit
 							},
 							SecurityContext: &corev1.SecurityContext{
 								AllowPrivilegeEscalation: pointer.Bool(false),
+								ReadOnlyRootFilesystem:   pointer.Bool(true),
 								Capabilities: &corev1.Capabilities{
 									Drop: []corev1.Capability{"ALL"},
 								},

deckhouse/3p-kubevirt

pkg/virt-controller/services/rendercontainer.go
@@ -213,10 +213,11 @@ func xdgEnvironmentVariables() []k8sv1.EnvVar {
 func securityContext(userId int64, privileged bool, requiredCapabilities *k8sv1.Capabilities) *k8sv1.SecurityContext {
 	isNonRoot := userId != 0
 	context := &k8sv1.SecurityContext{
-		RunAsUser:    &userId,
-		RunAsNonRoot: &isNonRoot,
-		Privileged:   &privileged,
-		Capabilities: requiredCapabilities,
+		RunAsUser:              &userId,
+		RunAsNonRoot:           &isNonRoot,
+		Privileged:             &privileged,
+		Capabilities:           requiredCapabilities,
+		ReadOnlyRootFilesystem: pointer.P(true),
 	}
 
 	if isNonRoot {

pkg/virt-controller/services/serialconsolelog.go
@@ -38,6 +38,7 @@ func generateSerialConsoleLogContainer(vmi *v1.VirtualMachineInstance, image str
 				RunAsUser:                pointer.P(int64(util.NonRootUID)),
 				RunAsNonRoot:             pointer.P(true),
 				AllowPrivilegeEscalation: pointer.P(false),
+				ReadOnlyRootFilesystem:   pointer.P(true),
 				Capabilities: &k8sv1.Capabilities{
 					Drop: []k8sv1.Capability{"ALL"},
 				},

pkg/virt-controller/services/template.go
@@ -985,6 +985,7 @@ func (t *templateService) RenderHotplugAttachmentPodTemplate(volumes []*v1.Volum
 					Resources: hotplugContainerResourceRequirementsForVMI(t.clusterConfig),
 					SecurityContext: &k8sv1.SecurityContext{
 						AllowPrivilegeEscalation: pointer.P(false),
+						ReadOnlyRootFilesystem:   pointer.P(true),
 						RunAsNonRoot:             pointer.P(true),
 						RunAsUser:                &runUser,
 						SeccompProfile: &k8sv1.SeccompProfile{
@@ -1125,6 +1126,7 @@ func (t *templateService) RenderHotplugAttachmentTriggerPodTemplate(volume *v1.V
 					Command:   command,
 					Resources: hotplugContainerResourceRequirementsForVMI(t.clusterConfig),
 					SecurityContext: &k8sv1.SecurityContext{
+						ReadOnlyRootFilesystem:   pointer.P(true),
 						AllowPrivilegeEscalation: pointer.P(false),
 						RunAsNonRoot:             pointer.P(true),
 						RunAsUser:                &runUser,
@@ -1237,6 +1239,7 @@ func (t *templateService) RenderExporterManifest(vmExport *exportv1.VirtualMachi
 					},
 					SecurityContext: &k8sv1.SecurityContext{
 						AllowPrivilegeEscalation: pointer.P(false),
+						ReadOnlyRootFilesystem:   pointer.P(true),
 						Capabilities:             &k8sv1.Capabilities{Drop: []k8sv1.Capability{"ALL"}},
 					},
 					Resources: vmExportContainerResourceRequirements(t.clusterConfig),
@@ -1428,6 +1431,7 @@ func generateContainerSecurityContext(selinuxType string, container *k8sv1.Conta
 	}
 	container.SecurityContext.SELinuxOptions.Type = selinuxType
 	container.SecurityContext.SELinuxOptions.Level = "s0"
+	container.SecurityContext.ReadOnlyRootFilesystem = pointer.P(true)
 }
 
 func (t *templateService) generatePodAnnotations(vmi *v1.VirtualMachineInstance) (map[string]string, error) {

pkg/virt-controller/services/template_test.go
@@ -4148,6 +4148,7 @@ var _ = Describe("Template", func() {
 			runUser := int64(util.NonRootUID)
 			Expect(*pod.Spec.Containers[0].SecurityContext).To(Equal(k8sv1.SecurityContext{
 				AllowPrivilegeEscalation: pointer.P(false),
+				ReadOnlyRootFilesystem:   pointer.P(true),
 				RunAsNonRoot:             pointer.P(true),
 				RunAsUser:                &runUser,
 				SeccompProfile: &k8sv1.SeccompProfile{
@@ -4289,6 +4290,7 @@ var _ = Describe("Template", func() {
 			runUser := int64(util.NonRootUID)
 			Expect(*pod.Spec.Containers[0].SecurityContext).To(Equal(k8sv1.SecurityContext{
 				AllowPrivilegeEscalation: pointer.P(false),
+				ReadOnlyRootFilesystem:   pointer.P(true),
 				RunAsNonRoot:             pointer.P(true),
 				RunAsUser:                &runUser,
 				SeccompProfile: &k8sv1.SeccompProfile{

pkg/virt-controller/services/virtiofs.go
@@ -136,6 +136,7 @@ func generateContainerFromVolume(volume *v1.Volume, image string, resources k8sv
 			RunAsGroup:               pointer.P(int64(util.NonRootUID)),
 			RunAsNonRoot:             pointer.P(true),
 			AllowPrivilegeEscalation: pointer.P(false),
+			ReadOnlyRootFilesystem:   pointer.P(true),
 			Capabilities: &k8sv1.Capabilities{
 				Drop: []k8sv1.Capability{
 					"ALL",

pkg/virt-operator/resource/generate/components/deployments.go
@@ -325,6 +325,19 @@ func NewApiServerDeployment(namespace, repository, imagePrefix, version, product
 	attachCertificateSecret(&deployment.Spec.Template.Spec, VirtHandlerCertSecretName, "/etc/virt-handler/clientcertificates")
 	attachProfileVolume(&deployment.Spec.Template.Spec)
 
+	volume := corev1.Volume{
+		Name: "tmp",
+		VolumeSource: corev1.VolumeSource{
+			EmptyDir: &corev1.EmptyDirVolumeSource{},
+		},
+	}
+	volumeMount := corev1.VolumeMount{
+		Name:      "tmp",
+		MountPath: "/tmp",
+	}
+	deployment.Spec.Template.Spec.Volumes = append(deployment.Spec.Template.Spec.Volumes, volume)
+	deployment.Spec.Template.Spec.Containers[0].VolumeMounts = append(deployment.Spec.Template.Spec.Containers[0].VolumeMounts, volumeMount)
+
 	pod := &deployment.Spec.Template.Spec
 	pod.ServiceAccountName = ApiServiceAccountName
 	pod.SecurityContext = &corev1.PodSecurityContext{
@@ -381,6 +394,7 @@ func NewApiServerDeployment(namespace, repository, imagePrefix, version, product
 
 	container.SecurityContext = &corev1.SecurityContext{
 		AllowPrivilegeEscalation: pointer.P(false),
+		ReadOnlyRootFilesystem:   pointer.P(true),
 		Capabilities: &corev1.Capabilities{
 			Drop: []corev1.Capability{"ALL"},
 		},
@@ -480,6 +494,7 @@ func NewControllerDeployment(namespace, repository, imagePrefix, controllerVersi
 
 	container.SecurityContext = &corev1.SecurityContext{
 		AllowPrivilegeEscalation: pointer.P(false),
+		ReadOnlyRootFilesystem:   pointer.P(true),
 		Capabilities: &corev1.Capabilities{
 			Drop: []corev1.Capability{"ALL"},
 		},
@@ -621,6 +636,7 @@ func NewOperatorDeployment(namespace, repository, imagePrefix, version, verbosit
 							},
 							SecurityContext: &corev1.SecurityContext{
 								AllowPrivilegeEscalation: pointer.P(false),
+								ReadOnlyRootFilesystem:   pointer.P(true),
 								Capabilities: &corev1.Capabilities{
 									Drop: []corev1.Capability{"ALL"},
 								},