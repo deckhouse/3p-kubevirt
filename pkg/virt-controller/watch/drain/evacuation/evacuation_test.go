kubevirt/kubevirt

chore: change mount points to support ro mount points (#19)
- Change /usr/bin to /ver/run for container disk volume.
- Add prefix for container names:
  - container disk image volume
  - virt-launcher compute container
  - hotplug-disk container
  - container-disk-binary container
- Compare container name with HasSuffix to support already running VMs.

pkg/virt-controller/watch/drain/evacuation/evacuation_test.go
@@ -594,7 +594,7 @@ func newPod(vmi *v1.VirtualMachineInstance, name string, phase k8scorev1.PodPhas
 		Status: k8scorev1.PodStatus{
 			Phase: phase,
 			ContainerStatuses: []k8scorev1.ContainerStatus{
-				{Ready: false, Name: "compute", State: k8scorev1.ContainerState{Running: &k8scorev1.ContainerStateRunning{}}},
+				{Ready: false, Name: "d8v-compute", State: k8scorev1.ContainerState{Running: &k8scorev1.ContainerStateRunning{}}},
 			},
 		},
 	}

deckhouse/3p-kubevirt

pkg/virt-controller/watch/drain/evacuation/evacuation_test.go
@@ -589,7 +589,7 @@ func newPod(vmi *v1.VirtualMachineInstance, name string, phase k8sv1.PodPhase, o
 		Status: k8sv1.PodStatus{
 			Phase: phase,
 			ContainerStatuses: []k8sv1.ContainerStatus{
-				{Ready: false, Name: "compute", State: k8sv1.ContainerState{Running: &k8sv1.ContainerStateRunning{}}},
+				{Ready: false, Name: "d8v-compute", State: k8sv1.ContainerState{Running: &k8sv1.ContainerStateRunning{}}},
 			},
 		},
 	}