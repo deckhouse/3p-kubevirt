kubevirt/kubevirt

feat(core): read-only containers (#9)
Description
As part of the integrity control task, we make all containers read-only, including Kubevirt containers, Containerized data importer containers.

Why do we need it, and what problem does it solve?
These changes improve the ability for integrity checks. Improve security for containers.

What is the expected result?
All containers from the d8-virtualization namespace, as well as all containers running in a custom namespace, must be created read-only.

---------

Signed-off-by: Daniil Antoshin <daniil.antoshin@flant.com>

pkg/virtctl/guestfs/guestfs.go
@@ -464,6 +464,7 @@ func createLibguestfsPod(pvc, image, cmd string, args []string, kvm, isBlock boo
 	}
 	allowPrivilegeEscalation := false
 	containerSecurityContext := &corev1.SecurityContext{
+		ReadOnlyRootFilesystem:   pointer.Bool(true),
 		AllowPrivilegeEscalation: &allowPrivilegeEscalation,
 		Capabilities: &corev1.Capabilities{
 			Drop: []corev1.Capability{"ALL"},

deckhouse/3p-kubevirt

pkg/virtctl/guestfs/guestfs.go
@@ -404,6 +404,7 @@ func (c *guestfsCommand) createLibguestfsPod(client *K8sClient, ns, cmd string, 
 	}
 	allowPrivilegeEscalation := false
 	containerSecurityContext := &corev1.SecurityContext{
+		ReadOnlyRootFilesystem:   pointer.P(true),
 		AllowPrivilegeEscalation: &allowPrivilegeEscalation,
 		Capabilities: &corev1.Capabilities{
 			Drop: []corev1.Capability{"ALL"},