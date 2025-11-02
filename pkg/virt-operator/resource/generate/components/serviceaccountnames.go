5a04f5207cbfd12c9afb1c0ec7da9cd74e2551a9
Dmitry Lopatin <dmitry.lopatin@flant.com>
2025-11-02 22:46:27 +0300

    fix
    Signed-off-by: Dmitry Lopatin <dmitry.lopatin@flant.com>

--- pkg/virt-operator/resource/generate/components/serviceaccountnames.go
+++ pkg/virt-operator/resource/generate/components/serviceaccountnames.go
@@ -4,7 +4,7 @@ const (
 	ApiServiceAccountName                       = "kubevirt-internal-virtualization-apiserver"
 	ControllerServiceAccountName                = "kubevirt-internal-virtualization-controller"
 	ExportProxyServiceAccountName               = "kubevirt-internal-virtualization-exportproxy"
-	SynchronizationControllerServiceAccountName = "kubevirt-synchronization-controller"
+	SynchronizationControllerServiceAccountName = "kubevirt-internal-virtualization-synchronization-controller"
 	HandlerServiceAccountName                   = "kubevirt-internal-virtualization-handler"
 	OperatorServiceAccountName                  = "kubevirt-operator"
 
