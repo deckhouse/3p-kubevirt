8a42472e0453655954fcb5c5ec8f58c97ea9d124
Ivan Mikheykin <ivan.mikheykin@flant.com>
2026-01-15 19:13:14 +0300

    chore: add heritage=deckhouse label for Pods in user ns
    Support security hardening for Deckhouse system components implemented by https://github.com/deckhouse/deckhouse/pull/16749

--- pkg/virt-controller/services/template.go
+++ pkg/virt-controller/services/template.go
@@ -1023,6 +1023,7 @@ func (t *templateService) RenderHotplugAttachmentPodTemplate(volumes []*v1.Volum
 			},
 			Labels: map[string]string{
 				v1.AppLabel:                            hotplugDisk,
+				"heritage":                             "deckhouse",
 				"security.deckhouse.io/skip-pss-check": "true",
 				v1.ResourceQuotaExclusionLabel:          "true",
 			},
@@ -1201,6 +1202,7 @@ func (t *templateService) RenderHotplugAttachmentTriggerPodTemplate(volume *v1.V
 			},
 			Labels: map[string]string{
 				v1.AppLabel:                            hotplugDisk,
+				"heritage":                             "deckhouse",
 				"security.deckhouse.io/skip-pss-check": "true",
 				v1.ResourceQuotaExclusionLabel:          "true",
 			},
@@ -1709,6 +1711,7 @@ func podLabels(vmi *v1.VirtualMachineInstance, hostName string) map[string]strin
 	labels[v1.AppLabel] = "virt-launcher"
 	labels[v1.CreatedByLabel] = string(vmi.UID)
 	labels[v1.VirtualMachineNameLabel] = hostName
+	labels["heritage"] = "deckhouse"
 	return labels
 }
 
