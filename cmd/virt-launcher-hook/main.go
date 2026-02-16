7b687dad290c7a92d072d378d1edfbe2af5646ea
Daniil Loktev <lokt.daniil@gmail.com>
2026-02-16 18:59:25 +0300

    refactor libvirt hook
    Signed-off-by: Daniil Loktev <lokt.daniil@gmail.com>

---
+++ cmd/virt-launcher-hook/main.go
@@ -0,0 +1,135 @@
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright The KubeVirt Authors.
+ *
+ */
+
+// Libvirt hook binary for qemu.
+// Installed at /etc/libvirt/hooks/qemu.
+// Called by libvirt with: domain_name operation sub-operation [extra]
+// Domain XML is passed via stdin.
+package main
+
+import (
+	"bufio"
+	"encoding/xml"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+const (
+	conntrackHookSock = "/var/run/kubevirt/sockets/conntrack-hook.sock"
+	socketTimeout     = 1 * time.Second
+)
+
+type domainMetadata struct {
+	KubeVirt struct {
+		UID string `xml:"uid"`
+	} `xml:"http://kubevirt.io kubevirt"`
+}
+
+type domainXML struct {
+	Metadata domainMetadata `xml:"metadata"`
+}
+
+func main() {
+	log.SetFlags(0)
+	log.SetPrefix("qemu-hook: ")
+
+	if len(os.Args) < 4 {
+		log.Printf("expected at least 3 args, got %d", len(os.Args)-1)
+		os.Exit(0)
+	}
+
+	operation := os.Args[2]
+	subOperation := os.Args[3]
+
+	stdinData, _ := io.ReadAll(os.Stdin)
+
+	log.Printf("domain=%s operation=%s sub_operation=%s", os.Args[1], operation, subOperation)
+
+	// "started begin" fires on the destination after migration data transfer
+	// completes but before VM resumes. Used to gate conntrack injection.
+	if operation == "started" && subOperation == "begin" {
+		handleStartedBegin(stdinData)
+	}
+}
+
+func handleStartedBegin(xmlData []byte) {
+	vmiUID, err := parseVMIUID(xmlData)
+	if err != nil {
+		log.Printf("failed to parse domain XML: %v", err)
+		return
+	}
+	if vmiUID == "" {
+		return
+	}
+
+	if err := waitForConntrackSync(vmiUID); err != nil {
+		log.Printf("conntrack sync error for VMI %s: %v", vmiUID, err)
+	}
+}
+
+func parseVMIUID(xmlData []byte) (string, error) {
+	var domain domainXML
+	if err := xml.Unmarshal(xmlData, &domain); err != nil {
+		return "", err
+	}
+	return domain.Metadata.KubeVirt.UID, nil
+}
+
+func waitForConntrackSync(vmiUID string) error {
+	if _, err := os.Stat(conntrackHookSock); err != nil {
+		return nil
+	}
+
+	conn, err := net.DialTimeout("unix", conntrackHookSock, socketTimeout)
+	if err != nil {
+		return fmt.Errorf("failed to connect: %w", err)
+	}
+	defer conn.Close()
+
+	conn.SetDeadline(time.Now().Add(socketTimeout))
+
+	if _, err := conn.Write([]byte("wait\n")); err != nil {
+		return fmt.Errorf("failed to send wait: %w", err)
+	}
+
+	response, err := readResponse(conn)
+	if err != nil {
+		return fmt.Errorf("failed to read response: %w", err)
+	}
+	if response != "ok" {
+		return fmt.Errorf("unexpected response: %s", response)
+	}
+
+	return nil
+}
+
+func readResponse(conn net.Conn) (string, error) {
+	scanner := bufio.NewScanner(conn)
+	if scanner.Scan() {
+		return scanner.Text(), nil
+	}
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+	return "", fmt.Errorf("no response")
+}
