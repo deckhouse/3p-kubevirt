58cd9475f912344e454d44bc59b93c627a7d81e1
Daniil Loktev <lokt.daniil@gmail.com>
2026-05-06 14:21:05 +0300

    wip
    Signed-off-by: Daniil Loktev <lokt.daniil@gmail.com>

--- pkg/host-disk/creator.go
+++ pkg/host-disk/creator.go
@@ -42,6 +42,7 @@ func (c diskImgCreator) CreateDiskAndSetOwnership(vmi *v1.VirtualMachineInstance
 	} else if !chownExisting {
 		return nil
 	}
+	// Change file ownership to the qemu user.
 	if err := ephemeraldiskutils.DefaultOwnershipManager.UnsafeSetFileOwnership(diskPath); err != nil {
 		log.Log.Reason(err).Errorf("Couldn't set Ownership on %s: %v", diskPath, err)
 		return err
