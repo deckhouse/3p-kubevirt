7a1d9253b466a9642cd43b3ddb714f12a5c60ec1
Ivan Mikheykin <ivan.mikheykin@flant.com>
2026-06-29 20:24:33 +0300

    ++ fix more "non-constant string" errors
    Signed-off-by: Ivan Mikheykin <ivan.mikheykin@flant.com>

--- pkg/virt-controller/watch/migration/migration.go
+++ pkg/virt-controller/watch/migration/migration.go
@@ -1223,7 +1223,7 @@ func (c *Controller) markMigrationAbortInVmiStatus(migration *virtv1.VirtualMach
 		if err != nil {
 			msg := fmt.Sprintf("failed to set MigrationState in VMI status. :%v", err)
 			c.recorder.Eventf(migration, k8sv1.EventTypeWarning, controller.FailedAbortMigrationReason, msg)
-			return fmt.Errorf("%s", msg)
+			return errors.New(msg)
 		}
 		log.Log.Object(vmi).Infof("Signaled migration %s/%s to be aborted.", migration.Namespace, migration.Name)
 		c.recorder.Eventf(migration, k8sv1.EventTypeNormal, controller.SuccessfulAbortMigrationReason, "Migration is ready to be canceled by virt-handler.")
