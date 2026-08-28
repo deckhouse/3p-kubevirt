kubevirt/kubevirt

fix race condition when hotplug volume status gets stuck in Detaching even though the associated pvc and attachment pod are bound and ready.
Signed-off-by: dsanatar <dsanatar@redhat.com>

pkg/virt-controller/watch/vmi/storage.go
@@ -173,15 +173,13 @@ func (c *Controller) processHotplugVolumeStatus(
 		statusCopy.HotplugVolume = &virtv1.HotplugVolumeStatus{}
 	}
 
+	usePVCStatus := false
+
 	if attachmentPod == nil {
 		if !c.volumeReady(statusCopy.Phase) {
 			statusCopy.HotplugVolume.AttachPodUID = ""
 			// Volume is not hotplugged in VM and Pod is gone, or hasn't been created yet, check for the PVC associated with the volume to set phase and message
-			phase, reason, message := c.getVolumePhaseMessageReason(pvcName, vmi.Namespace)
-			statusCopy.Phase = phase
-			log.Log.V(3).Infof("Setting phase %s for volume %s", phase, volumeName)
-			statusCopy.Message = message
-			statusCopy.Reason = reason
+			usePVCStatus = true
 		}
 	} else {
 		statusCopy.HotplugVolume.AttachPodName = attachmentPod.Name
@@ -198,6 +196,22 @@ func (c *Controller) processHotplugVolumeStatus(
 			statusCopy.Reason = controller.SuccessfulCreatePodReason
 			c.recorder.Eventf(vmi, k8sv1.EventTypeNormal, statusCopy.Reason, statusCopy.Message)
 		}
+		// Handle race condition where an unplugged volume was quickly re-attached.
+		// The old attachment pod may still be cleaning up while the new one is created,
+		// causing the new volume to inherit HotplugVolumeDetaching status from the previous
+		// hotplug instead of being reset. We reset the phase based on PVC status so it can
+		// properly transition through the normal attach flow on the next reconcile.
+		if statusCopy.Phase == virtv1.HotplugVolumeDetaching {
+			usePVCStatus = true
+		}
+	}
+
+	if usePVCStatus {
+		phase, reason, message := c.getVolumePhaseMessageReason(pvcName, vmi.Namespace)
+		statusCopy.Phase = phase
+		log.Log.V(3).Infof("Setting phase %s for volume %s", phase, volumeName)
+		statusCopy.Message = message
+		statusCopy.Reason = reason
 	}
 
 	*status = *statusCopy

deckhouse/3p-kubevirt

(cherry picked from commit d1b7b5650f6a1ff8ad67f4dcbdd91a6cd4638d92)
Signed-off-by: Daniil Antoshin <daniil.antoshin@flant.com>

pkg/virt-controller/watch/vmi/storage.go
@@ -250,6 +250,18 @@ func (c *Controller) updateVolumeStatus(vmi *virtv1.VirtualMachineInstance, virt
 					status.Reason = controller.SuccessfulCreatePodReason
 					c.recorder.Eventf(vmi, k8sv1.EventTypeNormal, status.Reason, status.Message)
 				}
+				// Handle race condition where an unplugged volume was quickly re-attached.
+				// The old attachment pod may still be cleaning up while the new one is created,
+				// causing the new volume to inherit HotplugVolumeDetaching status from the previous
+				// hotplug instead of being reset. We reset the phase based on PVC status so it can
+				// properly transition through the normal attach flow on the next reconcile.
+				if status.Phase == virtv1.HotplugVolumeDetaching {
+					phase, reason, message := c.getVolumePhaseMessageReason(&volume, vmi.Namespace)
+					status.Phase = phase
+					log.Log.V(3).Infof("Setting phase %s for volume %s", phase, volume.Name)
+					status.Message = message
+					status.Reason = reason
+				}
 			}
 		}
 